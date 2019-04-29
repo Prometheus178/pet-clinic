1032d6874611f9631f5fdc00ccca0c2a2d5b1ba5
siriusonestar <siriusonestar@yandex.ru>
2019-04-29 13:09:53 +0300

    added properties

--- pet-clinic-data/src/main/java/org/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/springframework/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package org.springframework.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
---
+++ pet-clinic-data/src/main/java/org/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.springframework.petclinic.services;
+
+import org.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
